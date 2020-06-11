7f7c5b4a92f4f1c6e037e14db41f7fa58f808da3
jesus <facundofreijomil@outlook.com>
2020-06-11 20:04:00 -0300

    cleaned code

--- src/display_sample.py
+++ src/display_sample.py
@@ -11,53 +11,61 @@ IP = "127.0.0.1"
 SERVER_PORT = 3001
 CLIENT_PORT = 9008
 
-WIDTH = 441000
-HEIGHT = 64
-EXTRA_LENGTH = 0
-S_LINE_WIDTH = 2
-PADDING = 5
+#Parametros de sample
+sample_values = []
+sample_size = []
+sample_width = 0
+sample_height = 0
 
+#---------------
+EXTRA_LENGTH = 0
+LINE_WIDTH = 2
+
+#Parametros de oled
+WIDTH = 128
+HEIGHT = 64
+
+#Inicializacion de oled
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=None)
 disp.begin()
 disp.clear()
 disp.display()
 
-y_list = []
-sample_size = []
 def pixel_handler(address, *value):
 
     if (address == "/sample/pos"):
-        y_list.append(value[0])
+        sample_values.append(value[0])
         
     if (address == "/sample/state"):
         if (value[0] == 1):
-            y_list.clear()
-            
+            sample_values.clear()
             print("STATE 1")
-        elif(value[0] == 2):
-            
-            disp.clear()
 
-            HEIGHT = len(y_list)
-            PADDING = HEIGHT / 10
-            image = Image.new(mode = "1", size = (len(y_list)+EXTRA_LENGTH, HEIGHT))
-            draw = ImageDraw.Draw(image) 
-            np_y = np.array(y_list)
-            y_scaled = np.interp(np_y, (np_y.min(), np_y.max()), (HEIGHT - PADDING, 0 + PADDING))
+        elif(value[0] == 2):
+            disp.clear()
             print("STATE 2")
             print(sample_size)
 
-            for i in range(len(y_list)-1):
-                y = int(y_scaled[i])
-                y_2 = int(y_scaled[i+1])
-                draw.line(((i, y),(i+1, y_2)), 1, S_LINE_WIDTH)
+            sample_height = len(sample_values)
+            PADDING = sample_height / 10
+            
+            image = Image.new(mode = "1", size = (sample_height + EXTRA_LENGTH, sample_height))
+            draw = ImageDraw.Draw(image) 
+            npval = np.array(sample_values)
+            smpl_scaled = np.interp(npval, (npval.min(), npval.max()), (sample_height - PADDING, 0 + PADDING))
 
-            # draw.line((len(y_scaled), 32, len(y_scaled)+EXTRA_LENGTH, 32), S_LINE_WIDTH)
-            resized = image.resize((128, 64))
-            b_n_w = resized.convert('1')
-            # b_n_w.save(filename) 
-                
-            disp.display(b_n_w)
+            for i in range(len(sample_values)-1):
+                point_a = (i, int(smpl_scaled[i]))
+                point_b = (i + 1, int(smpl_scaled[i+1]))
+                draw.line((point_a, point_b), 1, LINE_WIDTH)
+
+            # draw.line((len(smpl_scaled), 32, len(smpl_scaled)+EXTRA_LENGTH, 32), LINE_WIDTH)
+            resized = image.resize((WIDTH, HEIGHT))
+            bnw = resized.convert('1')
+            # bnw.save(filename) 
+            disp.image(bnw)  
+            disp.display(bnw)
+
             sample_size.clear()
             draw.rectangle(((0, 0), WIDTH, HEIGHT), 0)
             
