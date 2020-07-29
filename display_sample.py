4782069fef9b6f72b16a79d97afa692e0c46ca7c
jesus <facundofreijomil@outlook.com>
2020-07-28 23:34:58 -0300

    updated and restructured

--- display_sample.py
+++ display_sample.py
@@ -2,20 +2,15 @@ from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
 from pythonosc.udp_client import SimpleUDPClient
 
-#import numpy as np
 from PIL import Image, ImageDraw
 
 # import Adafruit_SSD1306
 
 IP = "127.0.0.1"
 SERVER_PORT = 3001
-CLIENT_PORT = 9008
 
-#Parametros de sample TODO eliminar datos inutilizados
-sample_values = []
+#Parametros de sample
 sample_size = []
-sample_width = 0
-sample_height = 0
 
 #---------------
 FILENAME = "wavepic.png"
@@ -26,25 +21,24 @@ LINE_WIDTH = 2
 WIDTH = 128
 HEIGHT = 64
 MIDDLE = HEIGHT/2
-# #Inicializacion de oled
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=None)
-# disp.begin()
-# disp.clear()
-# disp.display()
+
+#Inicializacion de oled
+ disp = Adafruit_SSD1306.SSD1306_128_64(rst=None)
+ disp.begin()
+ disp.clear()
+ disp.display()
 
 def pointers(start_pos=0, end_pos=WIDTH):
     img = Image.open("output.png").convert('1')
     draw = ImageDraw.Draw(img)
     
-    point_a = ((start_pos, 0), (start_pos, HEIGHT))
-    point_b = ((end_pos, 0), (end_pos, HEIGHT))
+    line_a = ((start_pos, 0), (start_pos, HEIGHT))
+    line_b = ((end_pos, 0), (end_pos, HEIGHT))
     
-
-    draw.line(point_a, 1)
-    draw.line(point_b, 1)
+    draw.line(line_a, 1)
+    draw.line(line_b, 1)
     
     return img
-
 
 def pixel_handler(address, *value):
         
@@ -52,28 +46,28 @@ def pixel_handler(address, *value):
         state = value[0]
 
         if (state == 0):
-            print("State 0: Not recording")
+            pointer_a = int(value[1])
+            pointer_b = int(value[2])
+            
+            disp.clear()
+            print(pointer_a, pointer_b)
+          #  print(f"""State 0: Not Recording
+          #  - initial samplerate is -> {sample_size[0]}
+          #  - final samplerate is -> {sample_size[1]}""")
+
+            img = pointers(pointer_a, pointer_b)
+
+            draw = ImageDraw.Draw(img)
+            img.show()
+
+            disp.image(img)  
+            disp.display(img)
+
+            sample_size.clear()
 
         elif (state == 1):
             print("State 1: Recording!")
 
-        elif(state == 2):
-            # disp.clear()
-            print(f"""State 2: Read complete!
-            - initial samplerate is -> {sample_size[0]}
-            - final samplerate is -> {sample_size[1]}""")
-
-            img = pointers(50, 70)
-
-            draw = ImageDraw.Draw(img)
-            draw.rectangle((0, 0), (50, 50), 1)
-            img.show()
-
-            # disp.image(img)  
-            # disp.display(img)
-
-            sample_size.clear()
-            
             
     if (address == "/sample/size"):
          val = int(value[0])
@@ -84,6 +78,7 @@ def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
 
 dispatcher = Dispatcher()
+
 dispatcher.map("/sample*", pixel_handler)
 dispatcher.set_default_handler(default_handler)
 
